bf1a6f82000443f16053ed6f6e89a234729d3fe7
PankajDivgi <pankaj.divgi@gmail.com>
2019-01-24 15:18:24 +0530

    php logo

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "manikrony92@gmail.com";
+    $to = "contact@nvest.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $name2 = $_REQUEST['name2'];
@@ -15,7 +15,7 @@
 
     $subject = "You have a message from your Plumber Master.";
 
-    $logo = 'http://nasiruddin.com/demo/plumbing-master/images/logo.png';
+    $logo = 'https://storage.googleapis.com/rc-cloud-5b94d89f645f0000013804a5/Xy8nAfJHR7sREqvNk%2Fuploads%2Fgg3kkmdhthuAdgbsFinitialuser%2Finitialuser%2FfThhzgCTjTiWrxtxA?GoogleAccessId=rocketchat-cloud-deployment%40rocket-chat.iam.gserviceaccount.com&Expires=1548323359&Signature=i1GTa%2BY7UMFGLv5y8rXbKfwDjDUUdkvT8KoCu%2Fv71iAroP2srDceKE9KdKYj23oSqf8y1AddIudYogmlgCx7H8NU4ABeKtuKjLYcZN9GxhDtGLapB%2BXhs75A%2FkGMaIxEml7jFCyXLg4H9N8aOSKEJebz330Z%2Fzb%2FzgW5%2FT23d8Wwn33GQ6khr8imPznFHKookPOP0QsPAtseVCfsGL%2FUcRlebWM8KlwVyH1sr8rEDJw%2Bp4LFi6eCLRdVtlKeD8Ste1w0op1dvtUVIlPpQEsI1qydx%2FXn5GMSLhR19yM6Safvb19gA0btio%2BCp77mz5pqeilizrQQ4QoJp%2B1kBb1iwQ%3D%3D&response-content-disposition=inline';
     $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Bixcoin Master Mail</title></head><body>";
